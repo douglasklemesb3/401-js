d91cc57e688f05142f0805338ed2558a72c54892
douglas <douglasklemes0303@gmail.com>
2019-07-22 17:44:02 -0300

    Adicionar for de js

---
+++ script.js
@@ -0,0 +1,41 @@
+
+/*let nomeCapivara = "jubiscreuda"
+let contador = 0;
+
+ while(contador <10 ){
+     console.log(nomeCapivara);
+     contador ++;
+ }*/
+
+ /*for(let contador=0; contador<4; contador ++){
+    console.log(contador);
+ }*/
+
+/* let listaDeChamada = ["jusbiscleuda", "akida sorte", "natyele besta", "karen idiota", "trouxa"];
+
+ /*for(let menstruaçao in listaDeChamada){
+     console.log(menstruaçao)
+ }
+ for(let jujuba of listaDeChamada){
+     console.log(jujuba)
+ }*/
+
+/*
+ATIVIDADE
+let filmes = [
+             {nome:"rei leão", classificacao: 10, estilo: "animacao"},
+             {nome:"velozes e furiosos",classificacao:16, estilo:"açao"},
+             {nome:"ana belle", classificacao:41, estilo:"suspence"},
+             {nome:"moana", classificacao:100, estilo:"animacao"}
+]
+let nome =prompt("digite seu nome");
+let idade = prompt("digite sua idade");
+
+for(let filme of filmes){
+    if(idade >= filme.classificacao){
+    alert(` Olá, ${nome} pode assistir ${filme.nome}.`)
+}
+    else{
+        alert(`Olá, ${nome} não pode assistir ${filme.nome}.`)
+    }
+}*/
